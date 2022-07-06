185311a97e327ef5e88d7e5afe466ecf3881b823
SERG <serdgant66@mail.ru>
2022-07-06 16:45:02 +0300

    add_fails

---
+++ src/test/java/syndrome100JaCoCOTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class syndrome100JaCoCOTest {
+
+    @Test
+    void shoudFindMax() {
+        syndrome100JaCoCO service = new syndrome100JaCoCO();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shoudFindMaxaa(){
+        syndrome100JaCoCO service= new syndrome100JaCoCO();
+        long[] incomesInBillions ={-12, -5,-8, -8, -24, -3, 0, -6, -4, -11, -24};
+        long expected=0;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected,actual);
+    }
+
+
+
+}
